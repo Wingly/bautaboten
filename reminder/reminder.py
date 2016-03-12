42ee6097a10ab9fdef79089422035a2761c382ed
Erik Horlin <erik.horlin@gmail.com>
2016-03-12 11:04:17 +0100

    Added reminder module

---
+++ reminder/reminder.py
@@ -0,0 +1,22 @@
+from datetime import datetime
+
+
+class Reminder:
+	hasReminded = False
+	def __init__(self, sender, rawMessage):
+		splitData = rawMessage.split(' ', 3)
+		#0 will be the command
+		#1 will be the date
+		#2 will be the time
+		dt = datetime.strptime(splitData[1] + " " + splitData[2], "%d-%m-%y %H:%M")
+		self.remindString = sender +": " + splitData[3]
+		self.remindDate = dt
+
+	def checkReminder(self):
+		now = datetime.now()
+		print ("now " + str(now))
+		print ("remind " +str(self.remindDate))
+		if self.remindDate < now:
+			self.hasReminded = True
+			return self.remindString
+		return "!wait"
