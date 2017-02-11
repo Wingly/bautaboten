d4b9d12d04cb5752842f3d4f33c262c7434507e4
Erik Horlin <erik.horlin@gmail.com>
2017-02-11 12:24:28 +0100

    Markov can now save/load 2worddata from files
     -using json as loadformat

--- markov/markov.py
+++ markov/markov.py
@@ -2,6 +2,7 @@ import random
 import time
 import os
 import codecs
+import json
 def start(pipeEnd, channel):
     
     markovChain = MarkovChain(pipeEnd, channel)
@@ -14,23 +15,35 @@ class MarkovChain:
         self.pipeEnd = pipeEnd
         self.channel = channel
         self.wordbase = {}
+        self.twoWordBase = {}
         self.stopSigns = [".", "!" , "?"]
-        self.__readFromFile("savedData/markov/" + channel + ".data")
+        self.__readFromFile("savedData/markov/" + channel)
         self.nextSave = time.time() + 18000
+        self.allow1wordCoef = 10
 
     def __saveToFile(self, path):
-        saveFile = open(path, 'w', encoding='utf-8')
+        #one word save
+        saveFile = open(path  + ".data", 'w', encoding='utf-8')
         output = ""
         for key in self.wordbase:
             output += key + ":" + " ".join(word for word in self.wordbase[key]) +"\n"
         saveFile.write(output)  
         saveFile.close()
+        #2 word save
+        saveFile = open(path  + "2words.json", 'w', encoding='utf-8')
+        output = ""
+        saveDict = {}
+        #for key in self.twoWordBase:
+        output += json.dumps(self.twoWordBase, ensure_ascii=False)
+
+        saveFile.write(output)
+        saveFile.close()
     def __readFromFile(self, path):
-        if not os.path.isfile(path):
+        if not os.path.isfile(path + ".data"):
             print ("no previous markov savefile for " + self.channel)
             return
-        print ("Reading from " + path)
-        loadFile = open(path , 'r', encoding='utf-8')
+        #One word data
+        loadFile = open(path  + ".data" , 'r', encoding='utf-8')
         for line in loadFile:
             data = line.split(':', 1)
             #print ("adding subwords for " + data[0])
@@ -39,6 +52,13 @@ class MarkovChain:
                 self.wordbase[data[0]].append(word)
             if not self.wordbase[data[0]]:
                 self.wordbase[data[0]].append("")
+        loadFile.close()
+        #Two word data
+        if not os.path.isfile(path + "2words.json"):
+            print ("no previous markov 2word savefile for " + self.channel)
+            return
+        loadFile = open(path + "2words.json", 'r', encoding='utf-8')
+        self.twoWordBase = json.loads(loadFile.read())
         loadFile.close()
 
     def __generateSentence(self, startWord):
@@ -142,7 +162,7 @@ class MarkovChain:
             hasChanged = False
             while 1:
                 if time.time() > self.nextSave and hasChanged:
-                    self.__saveToFile("savedData/markov/" + self.channel + ".data")
+                    self.__saveToFile("savedData/markov/" + self.channel)
                     self.nextSave = time.time() + 18000
                     hasChanged = False
                 if not self.pipeEnd.poll(0.1):
